3595f69a84b563956d55f9934aafb9144c764339
Rob Franceschini <robert.franceschini@gmail.com>
2016-01-16 21:22:22 -0500

    Initial commit to own repo

---
+++ amazon/src/wishlist.php
@@ -0,0 +1,342 @@
+<?php
+
+/*
+ * Amazon Wish Lister
+ *
+ * URL: http://www.justinscarpetti.com/projects/amazon-wish-lister/
+ * URL: https://github.com/doitlikejustin/amazon-wish-lister
+ * 
+ * Author: Justin Scarpetti
+ * 
+ */
+// error_reporting(0);
+set_time_limit(60);
+require_once('phpquery.php');
+
+//?id=YOUR_AMAZON_ID
+//get the amazon id or force an ID if none is passed
+if(isset($_GET['id'])) $amazon_id = $_GET['id'];
+else $amazon_id = '37XI10RRD17X2';
+
+//?tld=AMAZON_COUNTRY
+//Set the regional variant of Amazon to use.  e.g `?tld=co.uk` or `?tld=de` or ?tld=com`. Defaults to `com`
+//Tested with: `ca`, `com`, `com.br`, `co.jp`, `co.uk`, `de`, `fr`, `in`, `it`
+//Currently no wishlists available for: `com.au`, `com.mx`, `es`, `nl` 
+if(isset($_GET['tld'])) $amazon_country = $_GET['tld'];
+else $amazon_country = 'com';
+
+//?reveal=unpurchased
+//checks what to reveal (unpurchased, all, or purchased)... defaults to unpurchased
+if($_GET['reveal'] == 'unpurchased') $reveal = 'reveal=unpurchased';
+elseif($_GET['reveal'] == 'all') $reveal = 'reveal=all';
+elseif($_GET['reveal'] == 'purchased') $reveal = 'reveal=purchased';
+else $reveal = 'reveal=unpurchased';
+
+//?sort=date
+//sorting options (date, title, price-high, price-low, updated, priority)
+if($_GET['sort'] == 'date') $sort = 'sort=dateadded';
+elseif($_GET['sort'] == 'priority') $sort = 'sort=priority';
+elseif($_GET['sort'] == 'title') $sort = 'sort=universal-title';
+elseif($_GET['sort'] == 'price-low') $sort = 'sort=universal-price';
+elseif($_GET['sort'] == 'price-high') $sort = 'sort=universal-price-desc';
+elseif($_GET['sort'] == 'updated') $sort = 'sort=last-updated';
+else $sort = 'sort=dateadded';
+
+//?tag=affiliate-tag
+//Set the affiliate tag - usually ends `-21`
+if(isset($_GET['tag'])) $affiliate_tag = $_GET['tag'];
+else $affiliate_tag = '';
+
+$baseurl = 'http://www.amazon.' . $amazon_country;
+$content = phpQuery::newDocumentFile("$baseurl/registry/wishlist/$amazon_id?$reveal&$sort&layout=standard");
+$i = 0;
+
+if($content == '')
+{
+    echo('ERROR');
+	die();
+}
+else
+{
+	//get all pages
+	//if the count of itemWrapper is > 0 it's the old wishlist
+	if(count(pq('tbody.itemWrapper')) > 0)
+	{
+		$pages = count(pq('.pagDiv .pagPage'));
+	}
+	//it's the new wishlist
+	else
+	{
+		$pages = count(pq('#wishlistPagination li[data-action="pag-trigger"]'));
+	}
+	
+	//if no "$pages" then only 1 page exists
+	if(empty($pages)) $pages=1;
+	
+	for($page_num=1; $page_num<=$pages; $page_num++)
+	{
+		$contents = phpQuery::newDocumentFile("$baseurl/registry/wishlist/$amazon_id?$reveal&$sort&layout=standard&page=$page_num");
+		
+		if($contents == '')
+		{
+			echo('ERROR');
+			die();
+		}
+		else
+		{
+			//get all items
+			$items = pq('tbody.itemWrapper'); 
+			
+			//if items exist (the let's use the old Amazon wishlist
+			if($items->html())
+			{
+				//loop through items
+				foreach($items as $item)
+				{
+					$check_if_regular = pq($item)->find('span.commentBlock nobr');	
+					
+					if($check_if_regular != '')
+					{	
+						//$array[$i]['array'] = pq($item)->html();
+						$array[$i]['num'] = $i + 1;
+						$array[$i]['name'] = text_prepare(pq($item)->find('span.productTitle strong a')->html());
+						$array[$i]['link'] = pq($item)->find('span.productTitle a')->attr('href');
+						$array[$i]['oldprice'] = pq($item)->find('span.strikeprice')->html();
+						$array[$i]['newprice'] = text_prepare(pq($item)->find('span[id^="itemPrice_"]')->html());
+						$array[$i]['dateadded'] = text_prepare(str_replace('Added', '', pq($item)->find('span.commentBlock nobr')->html()));
+						$array[$i]['priority'] = pq($item)->find('span.priorityValueText')->html();
+						$array[$i]['rating'] = pq($item)->find('span.asinReviewsSummary a span span')->html();
+						$array[$i]['totalratings'] = pq($item)->find('span.crAvgStars a:nth-child(2)')->html();
+						$array[$i]['comment'] = text_prepare(pq($item)->find('span.commentValueText')->html());
+						$array[$i]['picture'] = pq($item)->find('td.productImage a img')->attr('src');
+						$array[$i]['page'] = $page_num;
+						$array[$i]['ASIN'] = get_ASIN($array[$i]['link']);
+						$array[$i]['largeimage'] = get_large_ssl_image($array[$i]['picture']);
+						$array[$i]['affiliateurl'] = get_affiliate_link($array[$i]['ASIN']);
+						
+						$i++;
+					}
+				}
+			}
+			//if $items is empty, most likely the new Amazon HTML wishlist is being retrieved
+			//let's load a the new wishlist
+			else
+			{
+				$items = pq('.g-items-section div[id^="item_"]');
+				
+				//loop through items
+				foreach($items as $item)
+				{
+					$name = htmlentities(trim(pq($item)->find('a[id^="itemName_"]')->html()));
+					$link = pq($item)->find('a[id^="itemName_"]')->attr('href');
+					
+					if(!empty($name) && !empty($link))
+					{
+						$total_ratings = pq($item)->find('div[id^="itemInfo_"] div:a-spacing-small:first a.a-link-normal:last')->html();
+						$total_ratings = trim(str_replace(array('(', ')'), '', $total_ratings));
+						$total_ratings = is_numeric($total_ratings) ? $total_ratings : '';
+						
+						//$array[$i]['array'] = pq($item)->html();
+						$array[$i]['num'] = $i + 1;
+						$array[$i]['name'] = $name;
+						$array[$i]['link'] = $baseurl . $link;
+						$array[$i]['oldprice'] = 'N/A';
+						$array[$i]['newprice'] = text_prepare(pq($item)->find('span[id^="itemPrice_"]')->html());
+						$array[$i]['dateadded'] = text_prepare(str_replace('Added', '', pq($item)->find('div[id^="itemAction_"] .a-size-small')->html()));
+						$array[$i]['priority'] = text_prepare(pq($item)->find('span[id^="itemPriorityLabel_"]')->html());
+						$array[$i]['rating'] = 'N/A';
+						$array[$i]['totalratings'] = $total_ratings;
+						$array[$i]['comment'] = text_prepare((pq($item)->find('span[id^="itemComment_"]')->html()));
+						$array[$i]['picture'] = pq($item)->find('div[id^="itemImage_"] img')->attr('src');
+						$array[$i]['page'] = $page_num;
+						$array[$i]['ASIN'] = get_ASIN($array[$i]['link']);
+						$array[$i]['largeimage'] = get_large_ssl_image($array[$i]['picture']);
+						$array[$i]['affiliateurl'] = get_affiliate_link($array[$i]['ASIN']);
+						
+						$i++;
+					}
+				}
+			}
+		}	
+	}
+}
+
+//format the xml (old style)
+function xml_ecode($array) {
+	$xml = '';
+	if (is_array($array) || is_object($array)) {
+		foreach ($array as $key=>$value) {
+			if (is_numeric($key)) {
+				$key = 'item'; 
+			}
+			
+			//create the xml tags
+			$xml .= '<' . $key . '>' . xml_ecode($value) . '</' . $key . '>';
+		}
+	} else { $xml = htmlspecialchars($array, ENT_QUOTES); }
+	return $xml;
+}
+
+//Convert an array into valid XML
+//From http://stackoverflow.com/a/5965940/1127699
+function xml_encode($data, &$xml_data) {
+	foreach( $data as $key => $value ) 
+	{
+		if( is_array($value) ) 
+		{
+			if( is_numeric($key) )
+			{
+				$key = 'item'.$key; //Elements can't be purely numeric
+			}
+			$subnode = $xml_data->addChild($key);
+			xml_encode($value, $subnode);
+		} else {
+			$xml_data->addChild("$key",htmlspecialchars("$value"));
+		}
+	}
+
+	return $xml_data->asXML();
+}
+
+function rss_encode($data) {
+
+	global $baseurl;
+
+	//	Most recent item
+	//	Should really be RFC-822
+	$pubDate = $data[0]['dateadded'];
+
+	$link = htmlspecialchars("{$baseurl}/registry/wishlist/{$amazon_id}?{$reveal}&{$sort}&layout=standard");
+
+	$rss = '<?xml version="1.0" encoding="UTF-8" ?>
+			<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+				<channel>
+					<title>Amazon Wishlist</title>
+					<description></description>
+					<link>'.$link.'</link>
+					<language>en-gb</language>
+					<pubDate>'.$pubDate.'</pubDate>';
+
+	foreach( $data as $key => $value ) 
+	{
+		if( is_array($value) ) 
+		{
+			$rss .= '<item>
+						<title>'.$value['comment'].' '.$value['newprice'] .'</title>
+						<link>'.$value['affiliateurl'].'</link>
+						<description>
+							<![CDATA[
+								<a href="'.$value['affiliateurl'].'">'.
+									html_entity_decode($value['name']).'
+									<br/>
+									<img src="'.$value['largeimage'].'"/>
+								</a>
+							]]>
+						</description>
+						<pubDate>'.$value['dateadded'].'</pubDate>
+						<guid>'.$value['affiliateurl'].'</guid>
+					</item>';
+		}
+	}
+
+	$rss .= '</channel>
+			</rss>';
+
+	return $rss;
+}
+
+//Make sure the text is prepared for use on the web.
+function text_prepare($text) {
+	return trim($text);
+}
+
+//	Get the ASIN of the product
+function get_ASIN($url) {
+	/*
+		ASIN is a 10 character string - https://en.wikipedia.org/wiki/Amazon_Standard_Identification_Number
+		Typical URls
+		http://www.amazon.co.uk/dp/B00IKIKDOM/ref=wl_it_dp_v_nS_ttl/278-9067265-3899254?_encoding=UTF8&colid=15SCUHW5RONL6&coliid=ICBVF90YVEFF4&psc=1
+		http://www.amazon.co.jp/dp/B00OOD4RLC/ref=wl_it_dp_v_S_ttl/376-5041442-9894656?_encoding=UTF8&colid=HIOT27YDKDXW&coliid=I2199742B598MC
+		http://www.amazon.de/dp/B00TRQ0CSI/ref=wl_it_dp_v_nS_ttl/275-4926214-1753269?_encoding=UTF8&colid=X82UNL4VMFM9&coliid=I2G0K6DW0S1MHT
+		http://www.amazon.ca/dp/B00A7WDYYU/ref=wl_it_dp_v_nS_ttl/180-5102401-8253319?_encoding=UTF8&colid=3OR5VN6044A6I&coliid=I30UZD33GMHBK3
+		http://www.amazon.com/dp/B00DQYNKCM/ref=wl_it_dp_v_nS_ttl/191-5492771-2500240?_encoding=UTF8&colid=37XI10RRD17X2&coliid=ILV2H2MHRX7HU&psc=1
+		http://www.amazon.es/dp/B007RXO716/ref=wl_it_dp_v_nS_ttl/279-0188662-6542856?_encoding=UTF8&colid=1ORXZQUAJ8H96&coliid=I35SODJIWT2DTA
+		http://www.amazon.in/dp/B00E81GGGY/ref=wl_it_dp_v_nS_ttl/275-0633160-8128200?_encoding=UTF8&colid=1HCKFSCVFG2UW&coliid=IUH4Z2TCPKDF2
+		http://www.amazon.it/dp/B00Y0O5L6U/ref=wl_it_dp_v_nS_ttl/280-4610661-1922667?_encoding=UTF8&colid=2RPUB231AJ78D&coliid=IFOHKF3REGMUU&psc=1
+		http://www.amazon.com.br/dp/B00YSILJZU/ref=wl_it_dp_v_nS_ttl/177-3586976-1287133?_encoding=UTF8&colid=3OF5TPV1ZMWLM&coliid=I36RLGSQVQOUYH
+		http://www.amazon.fr/dp/B003FRXFWK/ref=wl_it_dp_v_S_ttl/275-1202652-6343230?_encoding=UTF8&colid=1EHE58O7QKWTH&coliid=I3EGD6MPOTC9GY
+	*/
+
+	//	Remove the Base URL and /dp/
+	global $baseurl;
+	$ASIN = str_replace($baseurl . "/dp/", '', $url);
+	//	Grab the ASIN
+	$ASIN = substr($ASIN, 0, 10);
+
+	return $ASIN;
+}
+
+function get_large_ssl_image($image_url) {
+	/*
+		Change	
+			http://ecx.images-amazon.com/images/I/41kWB4Z4PTL._SL250_.jpg
+		To
+			https://images-eu.ssl-images-amazon.com/images/I/41kWB4Z4PTL._SL2500_.jpg
+
+		Image URLs are always .com for some reason.
+	*/
+
+	$largeSSLImage = str_replace("http://ecx.images-amazon.com", 'https://images-eu.ssl-images-amazon.com', $image_url);
+	$largeSSLImage = str_replace("_.jpg", '0_.jpg', $largeSSLImage);
+
+	return $largeSSLImage;
+}
+
+function get_affiliate_link($AISN) {
+
+	/*
+		According to https://affiliate-program.amazon.co.uk/gp/associates/help/t5/a21
+
+		> So if you need to build a simple text link to a specific item on Amazon.co.uk, here is the link format you need to use:
+		http://www.amazon.co.uk/dp/ASIN/ref=nosim?tag=YOURASSOCIATEID
+
+		e.g. http://www.amazon.co.uk/dp/B00U7EXH72/ref=nosim?tag=shkspr-21
+
+		Is this the same for all countries?
+
+		Your Associate ID only workds with one country
+		https://affiliate-program.amazon.co.uk/gp/associates/help/t22/a13%3Fie%3DUTF8%26pf_rd_i%3Dassoc_he..
+	*/
+
+	global $baseurl, $affiliate_tag;
+	$affiliateURL = $baseurl . "/dp/" . $AISN . "/ref=nosim?tag=" . $affiliate_tag;
+
+	return $affiliateURL;
+}
+
+//?format=json
+//format the wishlist (json, xml, or php array object) defaults to json
+if($_REQUEST['format'] == 'json') { 
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode($array); 
+}
+elseif($_REQUEST['format'] == 'xml') { 
+	header('Content-Type: text/xml; charset=utf-8');
+	echo xml_ecode($array); 
+}
+elseif($_REQUEST['format'] == 'XML') { 
+	header('Content-Type: text/xml; charset=utf-8');
+	echo xml_encode($array, new SimpleXMLElement('<?xml version="1.0"?><data></data>')); 
+}
+elseif($_REQUEST['format'] == 'array') { 
+	header('Content-Type: text/html; charset=utf-8');
+	print_r($array); 
+}
+elseif($_REQUEST['format'] == 'rss') { 
+	header('Content-Type: application/rss+xml; charset=utf-8');
+	echo rss_encode($array); 
+}
+else { 
+	header('Content-Type: application/json; charset=utf-8');
+	echo json_encode($array); 
+}
---
+++ js/project.js
@@ -0,0 +1,138 @@
+$(document).ready(function () {
+
+    
+    // Toggles the menu button between hamburger and arrow
+    $('.hamburger, .arrow').click(function () {
+		$(this).toggleClass('hamburger arrow');
+		$('nav').toggle('slide', {direction:'left'}, 300);
+	})
+
+	//If window > 703, display sidebar
+	$(window).resize(function () {
+	    if ($(window).width() > 703) {
+	      $('nav').show()
+	    }
+	    else {
+	      $('nav').hide()
+	      $('.arrow').removeClass('arrow').addClass('hamburger')
+	    }
+  	})
+
+	// Read a page's GET URL variables and return them as an associative array.
+	function getUrlVars() {
+	    var vars = [], hash;
+	    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+	    for(var i = 0; i < hashes.length; i++)
+	    {
+	        hash = hashes[i].split('=');
+	        vars.push(hash[0]);
+	        vars[hash[0]] = hash[1];
+	    }
+	    return vars;
+	}
+
+
+	// Retrieves Wish List data via Amazon Wish Lister API by Justin Scarpetti, formats and inserts data onto page
+	function getItems (ID, country, sort, filter) {
+		$.getJSON('http://wishy-watchy.com/amazon/src/wishlist.php?id=' + ID + '&tld=' + country + '&sort=' + sort + '&reveal=' + filter, function( data ) {
+			$('section').empty()
+			if (data == null) {
+				$('section').append('<div class="full-text"><h2>No Wish List items found!</h2></div>')
+				$('#loader').hide()
+			} 
+			else {
+				$.each(data, function(i, item) {
+					$('section').append('<article><div class="table-cell"><a href="' + data[i].link + '" target="_blank"><img src="' + data[i].largeimage + '"></a></div><div class="table-cell"><h4><a href="' + data[i].link + '" target="_blank">' + data[i].name + '</a></h4><h4>Current Price: <span class="green">' + data[i].newprice + '</span></h4><p>Price when added: ' + data[i].oldprice + '</p><p>Date added: ' + data[i].dateadded + '</p><p class="comment">Comments: ' + data[i].comment + '</p><a href="' + data[i].link + '" target="_blank" class="btn">BUY</a></div></article>' );
+				});
+	            $('#loader').hide()
+        	}
+		});
+	}
+
+
+	// On page load, check query strings and run getItems function
+	var wishlistID = getUrlVars()['id'];
+	if (wishlistID == undefined) {
+		wishlistID = ''
+	}
+	$('#amazon_id').val(wishlistID)
+	
+	var country = getUrlVars()['tld'];
+	if (country == undefined) {
+		country = 'com'
+	}
+	$('#country').val(country)
+	
+	var sort = getUrlVars()['sortby'];
+	if (sort == undefined) {
+		sort = 'date'
+	}
+	$('#sort').val(sort)
+	
+	var filter = getUrlVars()['filterby'];
+	if (filter == undefined) {
+		filter = 'unpurchased'
+	}
+	$('#filter').val(filter)
+
+	if (wishlistID == '') {
+		$('section').append('<div class="full-text"><h5>Use the menu to enter an Amazon Wish List ID, select your options and hit Go!</h5><h5>To find the ID, go to any public wish list on amazon.com. In the url, copy the 12-character value after "wishlist/".</h5><h5>For example:</h5><h5>http://www.amazon.com/gp/registry/wishlist/<span style="background-color: yellow;">1LOD32NWDLJ1</span>/ref=cm_wl_list_o_6?</h5></div>')
+		$('#loader').hide()
+	}
+	else {
+		getItems(wishlistID, country, sort, filter)
+	}
+
+
+	// On submit, append query strings and redirect
+	$('#wishlist-id').submit(function (event) {
+		event.preventDefault()
+		var wishlistID = $('#amazon_id').val()
+		var country = $('#country').val()
+		var sort = $('#sort').val()
+		var filter = $('#filter').val()
+		var url = 'http://wishy-watchy.com/?id=' + wishlistID + '&tld=' + country + '&sortby=' + sort + '&filterby=' + filter
+		$(location).attr('href', url )
+		// var wishlistID = $('#amazon_id').val()
+		// getItems(wishlistID)
+	})
+
+
+	// Bookmark script
+	$('#bookmark-this').click(function(e) {
+		var bookmarkURL = window.location.href;
+	    var bookmarkTitle = document.title;
+
+	    if ('addToHomescreen' in window && window.addToHomescreen.isCompatible) {
+	      // Mobile browsers
+	      addToHomescreen({ autostart: false, startDelay: 0 }).show(true);
+	    } else if (window.sidebar && window.sidebar.addPanel) {
+	      // Firefox version < 23
+	      window.sidebar.addPanel(bookmarkTitle, bookmarkURL, '');
+	    } else if ((window.sidebar && /Firefox/i.test(navigator.userAgent)) || (window.opera && window.print)) {
+	      // Firefox version >= 23 and Opera Hotlist
+	      $(this).attr({
+	        href: bookmarkURL,
+	        title: bookmarkTitle,
+	        rel: 'sidebar'
+	      }).off(e);
+	      return true;
+	    } else if (window.external && ('AddFavorite' in window.external)) {
+	      // IE Favorite
+	      window.external.AddFavorite(bookmarkURL, bookmarkTitle);
+	    } else {
+	      // Other browsers (mainly WebKit - Chrome/Safari)
+	      alert('Press ' + (/Mac/i.test(navigator.userAgent) ? 'Cmd' : 'Ctrl') + '+D to bookmark this page.');
+	    }
+
+	    return false;
+  	});
+
+	// Refresh button
+	$('#refresh').click (function () {
+		location.reload();
+	});
+
+
+
+});
